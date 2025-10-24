56b6d60816102969ab641ee08fa7d712b3228283
Keyunge <keyunge.the.real@outlook.com>
2025-10-24 22:05:39 +0700

    Adding funny messages :)

--- calc.h
+++ calc.h
@@ -18,4 +18,17 @@ class bmi {
         std::cout << "Enter height: "; std::cin >> height;
         std::cout << "Enter weight: "; std::cin >> weight;
     };
+    void chech() {
+        if (bmi < 18) {std::cout << "You underweight! Here is your ice cream!";}
+        else if (bmi == 19) {std::cout << "You normal! Congratulations!";}
+        else if (bmi == 24) {std::cout << "You bubby! Go to the GUM!";}
+        else if (bmi == 40) {std::cout << "You fucking overweight! You`re so fat, so... fuck you!!!";}
+        else if (bmi > 19 && bmi < 23) {std::cout << "You normal! Congratulations!";}
+        else if (bmi > 24 && bmi < 39) {std::cout << "You bubby! Go to the GUM!";}
+        else if (bmi > 40) {std::cout << "You fucking overweight! You`re so fat, so... fuck you!!!";}
+        std::cout << std::endl;
+    };
+    void showi() {
+        std::cout << "Your BMI is " << bmi << std::endl;
+    };
 };
