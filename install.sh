a983709425c0308e5298c0bd59deb091586acab4
Keyunge <keyunge.the.real@outlook.com>
2025-10-25 11:13:10 +0700

    Fixed install script

--- install.sh
+++ install.sh
@@ -1,27 +1,36 @@
-#!/bin/bash
-
 # Check if clang is installed
-if ! command -v clang &> /dev/null; then
+if ! command -v clang >/dev/null 2>&1
+then
     echo "clang is not installed"
 # Compiling
 else
     echo "Compiling"
-    clang -o bmi main.cpp
+    clang++ -o bmi main.cpp
     sudo mv bmi /bin/
+    if ! command -v bmi >/dev/null 2>&1
+    then
+        echo "Error: bmi is not installed"
+        exit 1
+    else
+        echo "Installation complete! Success"
+        exit 0
+    fi
 fi
 
-if !  command -v gcc &> /dev/null; then
+if ! command -v gcc >/dev/null
+then
     echo "gcc is not installed"
     exit 1
 else
     echo "Compiling"
-    gcc -o bmi main.cpp
+    g++ -o bmi main.cpp
     sudo mv bmi /bin/
-fi
-
-if ! command -v bmi &> /dev/null; then
-    echo "Error: bmi is not installed"
-    exit 1
-else
-    echo "Installation complete! Success"
+    if ! command -v bmi >/dev/null 2>&1
+    then
+        echo "Error: bmi is not installed"
+        exit 1
+    else
+        echo "Installation complete! Success"
+        exit 0
+    fi
 fi
